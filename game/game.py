37090b80a736fd7d3baf1e0439915a8269eabf03
Gooty <gootyboy@icloud.com>
2025-06-20 15:29:39 -0400

    Random Math Questions

--- game/game.py
+++ game/game.py
@@ -39,25 +39,26 @@ def update():
     if keyboard.down and ship.y < HEIGHT:
         ship.y += 5
 
+    bullet_duration = abs(ship.y - (-50)) / HEIGHT
     if not bullet.visible:
         bullet.pos = (ship.x, ship.y - 50)
         bullet.visible = True
-        animate(bullet, tween='linear', duration=1, y=-50, on_finished=lambda: reset_bullet(bullet))
-    if not bullet_2.visible and timer > 1/3:
+        animate(bullet, tween='linear', duration=bullet_duration, y=-50, on_finished=lambda: reset_bullet(bullet))
+    if not bullet_2.visible and timer > 2:
         bullet_2.pos = (ship.x, ship.y - 50)
         bullet_2.visible = True
-        animate(bullet_2, tween='linear', duration=1, y=-50, on_finished=lambda: reset_bullet(bullet_2))
-    if not bullet_3.visible and timer > 2/3:
+        animate(bullet_2, tween='linear', duration=bullet_duration, y=-50, on_finished=lambda: reset_bullet(bullet_2))
+    if not bullet_3.visible and timer % 2 == 0:
         bullet_3.pos = (ship.x, ship.y - 50)
         bullet_3.visible = True
-        animate(bullet_3, tween='linear', duration=1, y=-50, on_finished=lambda: reset_bullet(bullet_3))
+        animate(bullet_3, tween='linear', duration=bullet_duration, y=-50, on_finished=lambda: reset_bullet(bullet_3))
 
 def reset_bullet(bullet_obj):
     bullet_obj.visible = False
 
 def timer_value():
     global timer
-    timer += 1/60
+    timer += 1
 
 def update_timer():
     clock.schedule_interval(timer_value, 1/60)
